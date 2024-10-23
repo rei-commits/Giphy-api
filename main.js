c8127674ef4c7ee32dc736b1879513d2a219ec23
Andreina <125166401+rei-commits@users.noreply.github.com>
2024-10-23 11:11:23 -0400

    made a small giphy page

---
+++ main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const BASE_URL = "https://api.giphy.com";
+  const API_KEY = "t30CTa865L2Q4pyc8vcUjD5eG0ehOttC";
+
+  const button = document.querySelector("button");
+  const giphyInput = document.querySelector("input");
+
+  button.addEventListener("click", async () => {
+    let giphy = giphyInput.value;
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/v1/gifs/search?api_key=${API_KEY}&q=${giphy}&limit=25`
+      );
+      // console.log(response.data.data);
+      let gif = response.data.data;
+
+      for (let i = 0; i < gif.length; i++) {
+        // grab url property from a certain size
+        let giphyLink = gif[i].images.fixed_height_small.url;
+        //  console.log(giphyLink);
+         // put in image tag and display on page
+        let NewImg = document.createElement("img");
+        NewImg.setAttribute("src", giphyLink);
+        document.querySelector("div").appendChild(NewImg);
+
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
